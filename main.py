65676335d1e0ef889766033b67e65e779c3924fb
snarv4 <snarvaez@gbmpaulovi.edu.ec>
2025-10-22 16:30:42 -0500

    Agregar main.py
    Cree un programa que se dedica a calcular el ultimo digito (digito verificador) de la cedula Ecuatoriana

---
+++ main.py
@@ -0,0 +1,16 @@
+cedula = ""
+coeficientes = [2,1,2,1,2,1,2,1,2]
+suma_total = 0
+for i in range(9):
+    digito = int(cedula[i])
+    producto = digito * coeficientes[i]
+    if producto >= 10:
+        producto -= 9 
+    suma_total += producto
+    residuo = suma_total % 10
+    if residuo == 0:
+        digito_verificador = 0
+    else: digito_verificador = 10 - residuo
+    print(digito_verificador)
+
+    
